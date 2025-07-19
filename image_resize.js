21b8c8e711663553fcaebde6b4873d256edd1508
harazukukaromi <80260534+harazukukaromi@users.noreply.github.com>
2025-07-19 16:05:47 +0700

    Add files via upload

---
+++ image_resize.js
@@ -0,0 +1,22 @@
+// Ambil elemen input dan gambar preview
+const input = document.getElementById('imageInput');
+const preview = document.getElementById('preview');
+
+// Event listener ketika file dipilih
+input.addEventListener('change', function () {
+  const file = this.files[0];
+
+  if (file && file.type.startsWith('image/')) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+      preview.style.display = 'block';
+    };
+
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = '#';
+    preview.style.display = 'none';
+  }
+});
